bd39f982b4de67e4aa6e1d376e95ab2911894526
Anonymous <anon@zg5>
2016-07-23 16:49:38 +0800

    fix a thingy

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ if($_POST[access_token]){
 
 $link = mysqli_connect('localhost','root','rootpower','globesmshandler');
 
-$query = "INSERT INTO users (access_token, subscriber_number) VALUES ('".$_POST[access_token]."', '".$_POST[subscriber_number]."');";
+$query = "INSERT INTO users (access_token, subscriber_number) VALUES ('".$_POST["ccess_token"]."', '".$_POST["subscriber_number"]."');";
 
 mysqli_query($link, $query);
 
