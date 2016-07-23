8aa8c875ad0ce7d5e7673b6d0f540a9ea2ddb21f
Anonymous <anon@zg5>
2016-07-23 19:11:12 +0800

    asd

--- recv.php
+++ recv.php
@@ -4,7 +4,9 @@ $obj=file_get_contents('php://input');
 
 $value = json_decode($obj,true);
 
-$columns = implode(", ",array_keys($value["inboundSMSMessageList"]["inboundSMSMessage"]));
+$columns = $value["inboundSMSMessageList"]["inboundSMSMessage"];
+
+$arco = json_decode($columns,true);
 
 $dateTime = $value["inboundSMSMessageList"]["inboundSMSMessage"]["dateTime"];
 $message = $value["inboundSMSMessage"]["message"];
@@ -13,7 +15,7 @@ $senderAddress = $value["senderAddress"];
 $link = mysqli_connect('localhost','root','rootpower','globesmshandler');
 
 #$query = "INSERT INTO kaways (timestomp, sender, message) VALUES ('".$dateTime."', '".$message."', '".$senderAddress."');";
-$query = "INSERT INTO debug (dump, valdump) VALUES ('".$obj."', '".$columns."');";
+$query = "INSERT INTO debug (dump, valdump) VALUES ('".$arco."', '".$columns."');";
 
 mysqli_query($link, $query);
 
