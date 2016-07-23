3d9d1d053e6aa6a68fc7a988c616eb021639dab7
Anonymous <anon@zg5>
2016-07-24 04:28:39 +0800

    asd

--- recv.php
+++ recv.php
@@ -6,11 +6,33 @@ $dateTime = $inbound["inboundSMSMessageList"]["inboundSMSMessage"][0]["dateTime"
 $message = $inbound["inboundSMSMessageList"]["inboundSMSMessage"][0]["message"];
 $senderAddress = $inbound["inboundSMSMessageList"]["inboundSMSMessage"][0]["senderAddress"];
 
-
-
 $break = explode(" ",$message);
 
-if($break[0] != "KAWAY"){
+#get auth token
+$link = mysqli_connect('localhost','root','rootpower','globesmshandler');
+$query = "SELECT access_token FROM users WHERE subscriber_number=".substr($senderAddress, 7);
+$result = mysqli_query($link, $query);
+$row = mysqli_fetch_assoc($result);
+$authtoken = $row["access_token"];
+
+
+if($break[0] = "whereami"){
+
+$searchurl = "https://devapi.globelabs.com.ph/location/v1/queries/location?access_token=".$authtoken."&address=".substr($senderAddress, 7)."&requestedAccuracy=200";
+$locjson = file_get_contents($searchurl);
+$locarray = json_decode($locjson, true);
+
+$long = $locarray["terminalLocationList"]["terminalLocation"]["currentLocation"]["longitude"];
+$lat = $locarray["terminalLocationList"]["terminalLocation"]["currentLocation"]["latitude"];
+
+$resolveurl = "https://maps.googleapis.com/maps/api/geocode/json?latlng=".$lat.",".$long."&key=AIzaSyBCO1XKQt8S0AO6vebjeK2Uyrf_E27V6RE";
+$resolvejson = file_get_contents($resolveurl);
+$resolvearray = json_decode($resolvejson, true);
+$currentloc = $resolvearray["results"]["formatted_address"];
+
+$textreply = "You are at ".$currentloc;
+
+} else if($break[0] != "KAWAY"){
 	$textreply = "Invalid command. Text KAWAY, space, followed by a waiting shed code.";
 } else if (!$break[1]) { #SOME TEST ABOUT VALIDITY OF SHED CODE
 	$textreply = "Don't forget the shed code.";
@@ -32,12 +54,7 @@ $outbound["outboundSMSMessageRequest"]["address"][0] = $senderAddress;
 
 $payload = json_encode($outbound);
 
-#get auth token
-$link = mysqli_connect('localhost','root','rootpower','globesmshandler');
-$query = "SELECT access_token FROM users WHERE subscriber_number=".substr($senderAddress, 7);
-$result = mysqli_query($link, $query);
-$row = mysqli_fetch_assoc($result);
-$authtoken = $row["access_token"];
+
 
 $url = "https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/9331/requests?access_token=".$authtoken;
 #$url = "https://requestb.in/16v0czb1?access_token=".$authtoken;
