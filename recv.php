e8051642d00663083be941c93c386b722883c903
Anonymous <anon@zg5>
2016-07-23 19:27:34 +0800

    asd

--- recv.php
+++ recv.php
@@ -1,22 +1,14 @@
 <?php
 
-$obj=file_get_contents('php://input');
+$value = json_decode(file_get_contents('php://input'),true);
 
-$value = json_decode($obj,true);
-
-$columns = implode(", ",array_keys($value["inboundSMSMessageList"]["inboundSMSMessage"][0]));
-
-$escaped_values = array_map('mysql_real_escape_string', array_values($value["inboundSMSMessageList"]["inboundSMSMessage"][0]));
-$values  = implode(", ", $escaped_values);
-
-$dateTime = $value["inboundSMSMessageList"]["inboundSMSMessage"]["dateTime"];
-$message = $value["inboundSMSMessage"]["message"];
-$senderAddress = $value["senderAddress"];
+$dateTime = $value["inboundSMSMessageList"]["inboundSMSMessage"][0]["dateTime"];
+$message = $value["inboundSMSMessageList"]["inboundSMSMessage"][0]["message"];
+$senderAddress = $value["inboundSMSMessageList"]["inboundSMSMessage"][0]["senderAddress"];
 
 $link = mysqli_connect('localhost','root','rootpower','globesmshandler');
 
-#$query = "INSERT INTO kaways (timestomp, sender, message) VALUES ('".$dateTime."', '".$message."', '".$senderAddress."');";
-$query = "INSERT INTO debug (dump, valdump) VALUES ('".$values."', '".$columns."');";
+$query = "INSERT INTO kaways (timestomp, sender, message) VALUES ('".$dateTime."', '".$message."', '".$senderAddress."');";
 
 mysqli_query($link, $query);
 
