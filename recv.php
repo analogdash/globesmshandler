66f2461c1804da04450972947449ab4464ff55c8
Anonymous <anon@zg5>
2016-07-23 18:47:04 +0800

    test

--- recv.php
+++ recv.php
@@ -5,13 +5,13 @@ $obj=file_get_contents('php://input');
 $value = json_decode($obj,true);
 
 $dateTime = $value["inboundSMSMessageList"]["inboundSMSMessage"]["dateTime"];
-$message = $value["inboundSMSMessageList"]["inboundSMSMessage"]["message"];
-$senderAddress = $value["inboundSMSMessageList"]["inboundSMSMessage"]["senderAddress"];
+$message = $value["inboundSMSMessage"]["message"];
+$senderAddress = $value["senderAddress"];
 
 $link = mysqli_connect('localhost','root','rootpower','globesmshandler');
 
-#$query = "INSERT INTO kaways (timestamp, sender, message) VALUES ('".$dateTime."', '".$message."', '".$senderAddress."');";
-$query = "INSERT INTO debug (dump, valdump) VALUES ('".$obj."', '".$value."');";
+$query = "INSERT INTO kaways (timestomp, sender, message) VALUES ('".$dateTime."', '".$message."', '".$senderAddress."');";
+#$query = "INSERT INTO debug (dump, valdump) VALUES ('".$obj."', '".$value."');";
 
 mysqli_query($link, $query);
 
