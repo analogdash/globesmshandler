37fa4a73d2d3277f128216d13b425e806a50011e
Anonymous <anon@zg5>
2016-07-23 17:13:14 +0800

    modified:   test.php

--- test.php
+++ test.php
@@ -1,18 +1,17 @@
 <?php
 
-if($_POST[access_token]){
+$value = json_decode($_POST['json']);
+
+$tkn = $value["access_token"];
+
+$usernum = $value["subscriber_number"];
 
 $link = mysqli_connect('localhost','root','rootpower','globesmshandler');
 
-$query = "INSERT INTO users (access_token, subscriber_number) VALUES ('".$_POST["ccess_token"]."', '".$_POST["subscriber_number"]."');";
+$query = "INSERT INTO users (access_token, subscriber_number) VALUES ('".$tkn."', '".$usernum."');";
 
 mysqli_query($link, $query);
 
 mysqli_close();
-
-} else {
-
-	echo "NO data!";
-}
 
 ?>
