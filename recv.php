0ce040c9dd04609dee7c9fb8a032066e6cff682b
Anonymous <anon@zg5>
2016-07-24 02:00:33 +0800

    oo

--- recv.php
+++ recv.php
@@ -6,12 +6,14 @@ $dateTime = $value["inboundSMSMessageList"]["inboundSMSMessage"][0]["dateTime"];
 $message = $value["inboundSMSMessageList"]["inboundSMSMessage"][0]["message"];
 $senderAddress = $value["inboundSMSMessageList"]["inboundSMSMessage"][0]["senderAddress"];
 
+$break = explode(" ",$message);
+$shedcode = $break[1];
+
+
 $link = mysqli_connect('localhost','root','rootpower','globesmshandler');
-
-$query = "INSERT INTO kaways (timberr, sender, message) VALUES ('".$dateTime."', '".$message."', '".$senderAddress."');";
-
+$query = "INSERT INTO kaways (timberr, sender, message) VALUES ('".$shedcode."', '".$message."', '".$senderAddress."');";
 mysqli_query($link, $query);
-
 mysqli_close();
+
 
 ?>
