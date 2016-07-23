308bec51b9e1538b41ff6cb1496632d2f90c5e4b
Anonymous <anon@zg5>
2016-07-24 02:28:31 +0800

    asd

--- recv.php
+++ recv.php
@@ -9,11 +9,55 @@ $senderAddress = $value["inboundSMSMessageList"]["inboundSMSMessage"][0]["sender
 $break = explode(" ",$message);
 $shedcode = $break[1];
 
+if($break[0] != "KAWAY"){
+	$textreply = "Invalid command. Text KAWAY, space, followed by a waiting shed code.";
+} else if (1/*SOME TEST ABOUT VALIDITY OF SHED CODE*/) {
+	$textreply = "Invalid shed code. Please use a valide shed code.";
+} else if (0/*SOME TEST ABOUT UNSUCCESSFUL BACK END THING*/) {
+	$textreply = "Kaway failed, please try again.";
+} else if (0/*KAWAY WAS SUCCESSFUL*/){
+	$textreply = "Kawaii!";
+}
 
+## SEND STUFF
+
+##$value["outboundSMSMessageRequest"]["clientCorrelator"] = "888";
+$value["outboundSMSMessageRequest"]["senderAddress"] = "tel:8839";
+$value["outboundSMSMessageRequest"]["outboundSMSTextMessage"]["message"] = $textreply;
+$value["outboundSMSMessageRequest"]["address"][0] = $senderAddress;
+
+$payload = json_encode($value);
+
+#get auth token
 $link = mysqli_connect('localhost','root','rootpower','globesmshandler');
-$query = "INSERT INTO kaways (timberr, sender, message) VALUES ('".$shedcode."', '".$message."', '".$senderAddress."');";
+$query = "SELECT access_token FROM users WHERE subscriber_number=".substr($senderAddress, 7);
+$authtoken = mysqli_query($link, $query);
+
+#$url = "https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/8839/requests?access_token=".$authtoken;
+$url = "https://requestb.in/16v0czb1?access_token=".$authtoken;
+
+$ch = curl_init($url);
+
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+curl_setopt($ch, CURLOPT_POSTFIELDS, $payload );                                                             
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                 
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+    'Content-Type: application/json',                                                                                
+    'Content-Length: ' . strlen($payload))                                                                       
+);                                                                                                                   
+                                                                                                                     
+  $result = curl_exec($ch); 
+mysqli_close();
+  ## END send stuff
+
+
+
+
+
+/*
+$query = "INSERT INTO kaways (timberr, sender, message) VALUES ('".$shedcode."', '".$senderAddress."', '".$message."');";
 mysqli_query($link, $query);
 mysqli_close();
-
+*/
 
 ?>
