225c8ace99d215d44bbd4a82ab6b0fd1f9f899c5
Anonymous <anon@zg5>
2016-07-23 21:38:53 +0800

    Wo

--- send.php
+++ send.php
@@ -5,38 +5,20 @@ $value["outboundSMSMessageRequest"]["senderAddress"] = "tel:8839";
 $value["outboundSMSMessageRequest"]["outboundSMSTextMessage"]["message"] = "Hello World";
 $value["outboundSMSMessageRequest"]["Address"][0] = "tel:+639776519749";
 
-$curl_post_data = json_encode($value);
+$data_json = json_encode($value);
 
 #$url = "https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/8839/requests?access_token=69PVXqlqgQN5Ww_nd2KcKEnloEI_-Zt0wGLILZFKYBE"
 
 $url = "https://requestb.in/16v0czb1?access_token=69PVXqlqgQN5Ww_nd2KcKEnloEI_-Zt0wGLILZFKYBE"
 
-$curl = curl_init($url);
-
-
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_POST, true);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $curl_post_data);
-
-$curl_response = curl_exec($curl);
-
-if ($curl_response === false) {
-    $info = curl_getinfo($curl);
-    curl_close($curl);
-    die('error occured during curl exec. Additioanl info: ' . var_export($info));
-}
-
-curl_close($curl);
-
-$decoded = json_decode($curl_response);
-
-if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
-    die('error occured: ' . $decoded->response->errormessage);
-}
-
-echo 'response ok!<br>';
-
-var_export($decoded->response);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$data_json);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response  = curl_exec($ch);
+curl_close($ch);
 
 /*
 curl_setopt($ch, CURLOPT_URL, $url); 
