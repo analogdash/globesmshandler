912211bdcf04d859c1606669612b35d5035c7dd6
Anonymous <anon@zg5>
2016-07-23 18:00:11 +0800

    modified:   recv.php

--- recv.php
+++ recv.php
@@ -1,18 +1,17 @@
 <?php
 
-if($_POST[access_token]){
+$value = json_decode($_POST['json']);
+
+$dateTime = $value["inboundSMSMessageList"]["inboundSMSMessage"]["dateTime"];
+$message = $value["inboundSMSMessageList"]["inboundSMSMessage"]["message"];
+$senderAddress = $value["inboundSMSMessageList"]["inboundSMSMessage"]["senderAddress"];
 
 $link = mysqli_connect('localhost','root','rootpower','globesmshandler');
 
-$query = "INSERT INTO users (access_token, subscriber_number) VALUES ('".$_POST[access_token]."', '".$_POST[subscriber_number]."');";
+$query = "INSERT INTO kaways (timestamp, sender, message) VALUES ('".$dateTime."', '".$message."', '".$senderAddress."');";
 
 mysqli_query($link, $query);
 
 mysqli_close();
-
-} else {
-
-	echo "NO data!";
-}
 
 ?>
